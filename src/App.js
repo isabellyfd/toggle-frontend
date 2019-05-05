440b1847b98630243c08a88eb481e6fdaa620212
isadamascena <idamasce@thoughtworks.com>
2019-05-05 17:43:09 -0300

    Adds first test routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function Test1() {
+  return (<div> Test1 </div>);
+}
+
+function Test2() {
+  return (<div> Test2 </div>);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={Test1}></Route>
+        <Route path="/test" exact={true} component={Test2}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
