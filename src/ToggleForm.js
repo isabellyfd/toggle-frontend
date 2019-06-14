ea5af4a27ef03de1bbcd7103d2facc84abb2abb1
isadamascena <idamasce@thoughtworks.com>
2019-06-13 23:01:05 -0300

    adding form css to toggle form as well

--- src/ToggleForm.js
+++ src/ToggleForm.js
@@ -3,19 +3,23 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Switch from 'react-switch';
 
+import './form.css'
+
 const ToggleForm = ({checked, handleNewToggleName, handleNewToggleValue, handleNewToggleSubmit}) => {
     return (
-        <Form>
-            <Form.Group>
-                <Form.Label>Toggle Name</Form.Label>
-                <Form.Control placeholder="Enter an toggle name" onChange={handleNewToggleName}></Form.Control>
-            </Form.Group>
-            Value 
-            <br/>
-            <Switch onChange={handleNewToggleValue} checked={checked} />
-            <br/>
-            <Button variant="secondary" onClick={handleNewToggleSubmit}>Sumbit</Button>
-        </Form>
+        <div className="form">
+            <Form>
+                <Form.Group>
+                    <Form.Label>Toggle Name</Form.Label>
+                    <Form.Control placeholder="Enter an toggle name" onChange={handleNewToggleName}></Form.Control>
+                </Form.Group>
+                Value 
+                <br/>
+                <Switch onChange={handleNewToggleValue} checked={checked} />
+                <br/>
+                <Button variant="secondary" onClick={handleNewToggleSubmit}>Sumbit</Button>
+            </Form>
+        </div>
     );
 }
 
